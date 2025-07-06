592968fc51e0d9ba7314eaca268dcf05f8e1a277
Andy <tagrifavc@gmail.com>
2025-07-06 05:25:14 -0400

    added game link

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -1,8 +1,12 @@
 const DownloadButton = () => {
+    const handleClick = () => {
+        window.open("https://drive.google.com/file/d/1cSDwtxD3N1H52UGJQZNoXB8hFx3Q9X6t/view?usp=sharing");
+    };
+
     return (
         <div>
             <text style={{ color: "gray", paddingRight: 25 }}><i>alpha available</i></text>
-            <button>Download Now</button>
+            <button onClick={handleClick}>Download Now</button>
         </div>
     );
 }
