2d74f9dea2bc3ea5c44043aa75a17a7f5fb26dac
Andy <tagrifavc@gmail.com>
2025-08-17 00:30:19 -0400

    navbar scrolls on click

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,22 @@
-import "./NavBar.css"
+import "./NavBar.css";
 
 const NavBar = () => {
+    const scrollTo = (position: number = 0) => {
+        window.scrollTo({
+            top: position,
+            behavior: "smooth",
+        });
+    };
+
     return (
         <nav className="navbar">
             <ul className="navbar-links">
-                <li><a>DOWNLOAD</a></li>
-                <li><a>CHARACTERS</a></li>
+                <li onClick={() => {
+                    scrollTo();
+                    window.open("https://drive.google.com/file/d/1cSDwtxD3N1H52UGJQZNoXB8hFx3Q9X6t/view?usp=sharing")
+                }}>
+                <a>DOWNLOAD</a></li>
+                <li onClick={() => scrollTo(1050)}><a>CHARACTERS</a></li>
                 <li><a>ABOUT</a></li>
                 <li><a>CONTACT</a></li>
             </ul>
