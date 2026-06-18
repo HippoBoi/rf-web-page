81a0afd59ace4a2b4ecdb425f09202b3dd42a951
Andy <tagrifavc@gmail.com>
2026-06-17 21:52:51 -0400

    disable download button

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -1,13 +1,9 @@
 import "./DownloadButton.css";
 
 const DownloadButton = () => {
-    const handleClick = () => {
-        window.open("https://drive.google.com/file/d/1cSDwtxD3N1H52UGJQZNoXB8hFx3Q9X6t/view?usp=sharing");
-    };
-
     return (
         <div>
-            <button className="download-button" onClick={handleClick}>Download Now</button>
+            <button className="download-button" disabled>Coming Soon</button>
         </div>
     );
 }
